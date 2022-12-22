938d6633117d4d5b0626c7da70b908c7723fc1af
linjialiang <linjialiang@163.com>
2022-12-22 20:22:31 +0800

    update

---
+++ doc_handler/attacher.php
@@ -0,0 +1,201 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fw
+ * Date: 2018/12/21
+ * Time: 20:47
+ */
+
+//const docIn = 'D:\Temp\data\\';
+//const docOut = 'D:\Temp\out';
+const docIn = __DIR__.'/../raw/temp/';
+const docOut = __DIR__.'/../raw/phpstorm-stubs-2022.3/';
+const line = PHP_EOL;
+const dataArr = [
+    'AMQPBasicProperties.getContentType' => 'test comment',
+    'AMQP_NOPARAM' => 'test const',
+    'class.AMQPBasicProperties' => 'test class',
+];
+
+
+function myPrint(...$args)
+{
+	foreach ($args as $arg) {
+		print_r($arg);
+		echo PHP_EOL;
+	}
+}
+
+function my_dir($dir, $parent = '', &$files = [])
+{
+	myPrint($dir);
+    if (@$handle = opendir($dir)) { //注意这里要加一个@，不然会有warning错误提示：）
+	    while (($file = readdir($handle)) !== false) {
+		    if ($file != ".." && $file != ".") { //排除根目录
+			    if (is_dir($dir . "/" . $file)) { //如果是子文件夹，就进行递归
+				    my_dir($dir . "/" . $file, $parent . '/' . $file, $files);
+			    } else { //不然就将文件的名字存入数组
+				    $files[] = $parent . '/' . $file;
+			    }
+		    }
+	    }
+	    closedir($handle);
+	    return $files;
+    }
+}
+
+function isComment($line)
+{
+    foreach (['/*', '*', '*/','#'] as $item) {
+        if (strpos($line, $item) === 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function getComment($token, $oldComment)
+{
+    if (strpos($token, 'constant.') !== 0)  //不是常量替换下划线
+        $token = str_replace('_', '-', $token);
+    $file = docIn . $token . '.html';
+    $return='';
+    if (file_exists($file)) {
+        if ($oldComment) {  //保留return行
+            $olds = explode("\n", $oldComment);
+            foreach ($olds as $old) {
+                $old2 = trim($old);
+                if (strpos($old2, '* @return')===0){
+                    $return = $old;
+                    break;
+                }
+            }
+        }
+        $comment = file_get_contents($file);
+        $comment = '/**' . line . '*' . $comment . line . $return . '*/' . line;
+        return $comment;
+    } else {
+        return $oldComment;
+    }
+}
+
+function isElement($line, $type)
+{
+    $tokens = explode(' ', $line);
+    for ($i = 0; $i < count($tokens); $i++) {
+        if ($tokens[$i] == $type) {
+            $name = $tokens[$i + 1];
+            $name = trim($name);
+            if (strpos($name, '(')) {
+                $name = substr($name, 0, strpos($name, '('));
+            }
+            return $name;
+        }
+    }
+    return false;
+}
+
+function isClass($line)
+{
+    return isElement($line, 'class');
+}
+
+function isFunction($line)
+{
+    return isElement($line, 'function');
+}
+
+function isConst($line)
+{
+//    define('AMQP_EX_TYPE_HEADERS', 'headers');
+    $line = str_replace(' ', '', $line);
+    $pre = "define('";
+    if (strpos($line, $pre) === 0) {
+        $line = str_replace($pre, '', $line);
+        $const = explode("'", $line)[0];
+        return $const;
+    }
+    return false;
+}
+
+function isVar($line){
+    $line = str_replace(' ', '', $line);
+    $pre = "$";
+    if (strpos($line, $pre) === 0) {
+        $line = str_replace($pre, '', $line);
+        $line = str_replace('_', '', $line);
+        $var = explode("=", $line)[0];
+        return $var;
+    }
+    return false;
+}
+
+function handle($name)
+{
+    $file = docOut . $name;
+    $newContent = '';
+    $handle = fopen($file, "r");//以只读方式打开一个文件
+    $i = 0;
+    $comment = '';
+    $class = '';
+    while (!feof($handle)) {//函数检测是否已到达文件末尾
+        if ($line = fgets($handle)) {// 从文件指针中读取一行
+            $line1 = str_replace(' ', '', $line);
+
+            //注释
+            if (isComment($line1)) {
+                $comment .= $line;
+                continue;
+            }
+
+            //类
+            if ($clsName = isClass($line)) {
+                $class = $clsName;
+                $newComment = getComment('class.' . $class, $comment);
+                $newContent .= $newComment;
+                $comment = '';    //注释已使用
+            }else if ($function = isFunction($line)) {  //函数方法
+                if (substr($function,0,20)=='PS_UNRESERVE_PREFIX_'){
+                    $function=substr($function,20);
+                }
+                $blankPre = strpos($line, ' ') === 0;    //前面空白是类方法的特征
+                $function = $class && $blankPre ? $class . '.' . $function : 'function.' . $function;
+                $newComment = getComment($function, $comment);
+                $newContent .= $newComment;
+                $comment = '';    //注释已使用
+            }else if ($const = isConst($line)) {    //常量
+                $newComment = getComment('constant.' . $const, $comment);
+                $newContent .= $newComment;
+                $comment = '';    //注释已使用
+            }else if($var = isVar($line)){  //预定义变量
+                $newComment = getComment('reserved.variables.' . $var, $comment);
+                $newContent .= $newComment;
+                $comment = '';    //注释已使用
+            }
+
+            //没有匹配到任何类型内容
+            if ($comment) {
+                $newContent .= $comment;
+                $comment = '';
+            }
+            $newContent .= $line;
+        };
+    }
+    file_put_contents(docOut . $name, $newContent);
+}
+
+function handleAll()
+{
+    $files = [];
+    my_dir(docOut, '', $files);
+    foreach ($files as $file) {
+        $suffix = substr(strrchr($file, '.'), 1);
+        if ($suffix == 'php') {
+            handle($file);
+            echo $file . line;
+//            break;  //test
+        }
+    }
+}
+
+handleAll();
---
+++ doc_handler/handler.php
@@ -0,0 +1,242 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: f
+ * Date: 2018/12/20
+ * Time: 17:28
+ * 处理html
+ */
+include('./simple_html_dom.php');
+
+const line = PHP_EOL;
+const site = 'https://www.php.net/manual/zh/';    //外链站点 //也可设为本地衔接 例: file:///D:/Temp/php-chunked-xhtml/
+const in   = __DIR__.'/../raw/php-chunked-xhtml/';
+const temp = __DIR__.'/../raw/temp/';
+
+
+function myPrint(...$args)
+{
+	foreach ($args as $arg) {
+		print_r($arg);
+		echo PHP_EOL;
+	}
+}
+
+/**
+ * 加载文本内容
+ * @param $name
+ * @return bool|string
+ */
+function loadStr($name)
+{
+    $path = in . $name;
+    $content = file_get_contents($path) or die("Unable to open file!");
+    return $content;
+}
+
+/**
+ * 修改衔接
+ * @param simple_html_dom $dom
+ */
+function modifyUrl($dom)
+{
+    $links = $dom->find('a');
+	foreach ($links as $iValue) {
+	    $a = $iValue;
+	    $href = $a->href;
+	    if (strstr($href, 'http://')) {    //不处理外链
+	        continue;
+	    }
+
+	    $known = 0;
+	    if (strstr($href, 'function.')) {
+	        $known = 1;
+	    } else if (strstr($a->innertext, '::')) {
+	        $known = 1;
+	    }
+
+	    if ($known) {   //已知类型, 方法,类静态方法..
+	        $href = '{@link ' . $a->innertext . '}';
+	        $a->outertext = $href;
+	    } else {        //如果未匹配到任何类型, 改成官网外链
+	        $href = str_replace('.html', '.php', $href);    //网站外链为php 本地为html
+	        $a->href = site . $href;
+	    }
+	}
+}
+
+/**
+ * 修改文本
+ */
+function modifyStr($html)
+{
+    //防止注释异常终止
+    $html = str_replace('/*', '//', $html);
+    $html = str_replace('*/', '', $html);
+    //重设代码颜色以便在黑色主题下查看
+    $html = str_replace('#0000BB', '#9876AA', $html);
+    //清理换行
+    $html = str_replace("\r", '', $html);
+    $html = str_replace("\n", '', $html);
+    return $html;
+}
+
+function modifyAttr($dom,$selector,$value,$attr='style'){
+    $subs=$dom->find($selector);
+    foreach ($subs as $sub){
+        $sub->setAttribute($attr,$value);
+    }
+}
+
+function modifyTag($dom, $selector,$outside, $pre, $after,$one=false){
+    $subs=$dom->find($selector);
+    foreach ($subs as $sub){
+        if($outside)
+            $sub->outertext=$pre.$sub->outertext().$after;
+        else
+            $sub->outertext=$pre.$sub->innertext().$after;
+        if($one)
+            break;
+    }
+}
+
+function modifyOutput($dom){
+    $subs = $dom->find("pre");
+    foreach ($subs as $sub) {
+        $text = $sub->innertext();
+        if (substr($text,0,1)=="\n"){
+            $text=substr($text,1);
+        }
+        $text = '<span>' . str_replace("\n", "<br>",$text) . '</span>';
+        $sub->outertext = $text;
+    }
+}
+
+function handleStyle($dom){
+    //方法颜色
+    modifyAttr($dom,'.methodname','color:#CC7832');
+    modifyAttr($dom,'.function strong','color:#CC7832');
+//类型颜色
+    modifyAttr($dom,'.type','color:#EAB766');
+//参数颜色
+    modifyAttr($dom,'.parameter','color:#9070A1');
+//方法描述背景
+//    modifyAttr($dom,'.methodsynopsis','border:1px gray;padding-left:5px;background:#232525');
+//添加分隔符
+//modifyAttr($dom,"div[class='refsect1']","BORDER-TOP: gray 1px dashed; OVERFLOW: hidden; HEIGHT: 1px");
+//note
+    modifyAttr($dom,".note","border:1px gray solid");
+//php代码
+    modifyAttr($dom,".phpcode","border-color:gray;background:#232525");
+//output
+    modifyAttr($dom,".screen","color:AFB1B3;background:black;padding-left:5px;");
+
+//pre
+    modifyOutput($dom);
+//    modifyTag($dom,"pre",false,'<span>','</span>');
+//code
+    modifyTag($dom,"code",false,'<span>','</span>');
+//参数标签, 9070A1 编辑器紫, EE82EE 鲜艳紫, 00B5FF 鲜艳蓝,4285F4 一般蓝, 19A1FA 3A95FF ok蓝
+    modifyTag($dom,'.parameter',false,'<span class="parameter" style="color:#3A95FF">','</span>');
+//去除换行:参数,示例
+    modifyTag($dom,".parameters .para",false,'<span>','</span>',true);
+    modifyTag($dom,".examples .para",false,'<span>','</span>',true);
+    modifyTag($dom,".seealso .para",false,'<span>','</span>',true);
+    modifyTag($dom,".changelog .para",false,'<span>','</span>',true);
+//添加分隔符,换行标签
+    modifyTag($dom,"div[class='refsect1']",true,'<br></br><div style="BORDER-TOP: gray 1px dashed; OVERFLOW: hidden; HEIGHT: 1px"></div>','');
+//modifyTag($dom,"div[class='refsect1']",true,'<br></br><hr></hr>','');
+    return $dom;
+}
+
+function handleConst($file = 'filesystem.consts.html')
+{
+    $content = loadStr($file);
+    $selector = str_get_html($content);
+    $name = substr($file, 0, strlen($file) - 5);
+
+    $doms = $selector->find('strong code');
+    foreach ($doms as $dom) {
+//        $name = $dom->find('strong code', 0);
+        if (!$dom) continue;
+        $outFile = 'constant.' . $dom->innertext . '.html';
+        $parent = $dom->parentNode()->parentNode();
+        /** @var simple_html_dom_node $test */
+        $next = $parent->nextSibling();
+        if (!$next) continue;
+        $next = $next->children(0);
+        if (!$next) continue;
+        modifyUrl($next);
+        $html = $next->innertext;
+        if (!$html) continue;
+        if (trim($html) == '') continue;
+        $html = modifyStr($html);
+        if (strpos($outFile, '::')) continue;
+        echo $outFile . line;
+        file_put_contents(temp . '/' . $outFile, $html);
+    }
+}
+
+function getClass()
+{
+    $clses = [];
+    if (@$handle = opendir(in)) {
+        while (($file = readdir($handle)) !== false) {
+            $pre = 'class.';
+            if ((substr($file, 0, strlen($pre)) === $pre)) {
+                $clsName = substr($file, strlen($pre), strlen($file) - strlen($pre) - 5);
+                $clses[$clsName] = 1;
+            }
+        }
+    }
+    return $clses;
+}
+
+/**
+ * 处理函数
+ */
+function handle($file = 'function.date.html')
+{
+    $content = loadStr($file);
+    $selector = str_get_html($content,true,true,DEFAULT_TARGET_CHARSET,false);
+    $name = substr($file, 0, strlen($file) - 5);
+    $dom = $selector->find("div[id='$name']", 0);
+
+//    $ref = $selector->find("div[class='up']", 0);  //参考衔接
+    modifyUrl($dom);
+//    modifyUrl($ref);
+//    $dom->appendChild($ref);
+    handleStyle($dom);
+
+    $html = $dom->outertext;
+    $html = modifyStr($html);
+
+    file_put_contents(temp . '/' . $file, $html);
+    echo $file . line;
+}
+
+function handleAll()
+{
+	if (! is_dir(temp)) {
+		mkdir(temp);
+	}
+    $clses = getClass();
+    $clses['function'] = 1;
+    $clses['class'] = 1;
+    $clses['reserved'] = 1;
+    if (@$handle = opendir(in)) {
+	    while (($file = readdir($handle)) !== false) {
+	        $tokens = explode('.', $file);
+	        $prefix = $tokens[0];
+            if (@$clses[$prefix]) {
+                handle($file);
+            }
+            if ($tokens[count($tokens) - 2] == 'constants') {  //收集常量
+                handleConst($file);
+            }
+        }
+    }
+}
+
+//handle('function.substr.html');
+handleAll();
