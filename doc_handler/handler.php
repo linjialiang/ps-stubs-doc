cae7ce4ef24848a3eb85ba21ef17b62eba51e8e5
linjialiang <linjialiang@163.com>
2023-04-29 17:52:54 +0800

    update

--- doc_handler/handler.php
+++ doc_handler/handler.php
@@ -1,11 +1,5 @@
 <?php
 
-/**
- * 处理 HTML
- */
-
-include './simple_html_dom.php';
-
 const LINE = "\n";
 
 /**
@@ -23,6 +17,11 @@ const IN_PATH = __DIR__ . '/../raw/php-chunked-xhtml/';
  * 临时文件存放目录
  */
 const TEMP_PATH = __DIR__ . '/../raw/temp/';
+
+/**
+ * 日志路径
+ */
+const LOG_PATH = __DIR__ . '/../raw/log/';
 
 
 function myPrint(...$args)
@@ -127,53 +126,8 @@ function modifyOutput($dom)
     }
 }
 
-function handleStyle($dom)
-{
-    // 方法颜色
-    modifyAttr($dom, '.methodname', 'color:#CC7832');
-    modifyAttr($dom, '.function strong', 'color:#CC7832');
-    // 类型颜色
-    modifyAttr($dom, '.type', 'color:#EAB766');
-    // 参数颜色
-    modifyAttr($dom, '.parameter', 'color:#9070A1');
-    // 方法描述背景，源码隐藏
-    // modifyAttr($dom, '.methodsynopsis', 'border:1px gray;padding-left:5px;background:#232525');
-    // 添加分隔符，源码隐藏
-    // modifyAttr($dom, "div[class='refsect1']", "BORDER-TOP: gray 1px dashed; OVERFLOW: hidden; HEIGHT: 1px");
-    // note
-    modifyAttr($dom, ".note", "border:1px gray solid");
-    // php代码
-    modifyAttr($dom, ".phpcode", "border-color:gray;background:#232525");
-    // output
-    modifyAttr($dom, ".screen", "color:AFB1B3;background:black;padding-left:5px;");
-    // pre
-    modifyOutput($dom);
-    // 源码隐藏
-    // modifyTag($dom, "pre", false, '<span>', '</span>');
-    // code
-    modifyTag($dom, "code", false, '<span>', '</span>');
-    // 参数标签, 9070A1 编辑器紫, EE82EE 鲜艳紫, 00B5FF 鲜艳蓝,4285F4 一般蓝, 19A1FA 3A95FF ok蓝
-    modifyTag($dom, '.parameter', false, '<span class="parameter" style="color:#3A95FF">', '</span>');
-    // 去除换行:参数,示例
-    modifyTag($dom, ".parameters .para", false, '<span>', '</span>', true);
-    modifyTag($dom, ".examples .para", false, '<span>', '</span>', true);
-    modifyTag($dom, ".seealso .para", false, '<span>', '</span>', true);
-    modifyTag($dom, ".changelog .para", false, '<span>', '</span>', true);
-    // 添加分隔符,换行标签
-    modifyTag($dom, "div[class='refsect1']", true, '<br><div style="BORDER-TOP: gray 1px dashed; OVERFLOW: hidden; HEIGHT: 1px"></div>', '');
-    // modifyTag($dom, "div[class='refsect1']", true, '<br></br><hr></hr>', '');
-    return $dom;
-}
-
 function handleAll()
 {
-    $file = 'class.addressinfo.html';
-    $dom = load_dom($file);
-    // 获取所需节点
-    $node = $dom->getElementById(substr($file, 0, strlen($file) - 5));
-    $content = preg_replace('/ *\n */', '', $dom->saveHTML($node));
-    save_file($file, $content);
-
     if (!is_dir(TEMP_PATH)) mkdir(TEMP_PATH);
     $manHandle = @opendir(IN_PATH); // 开打手册目录句柄
     if (!$manHandle) exit('目录打开失败');
@@ -182,70 +136,62 @@ function handleAll()
         if (is_file(IN_PATH . $file)) {
             $tokens = explode('.', $file);
             // 处理函数、类、保留字
-            if (in_array($tokens[0], $typeList)) {
-                $dom = load_dom($file);
-                // 获取所需节点
-                $dom->getElementById(substr($file, 0, strlen($file) - 5));
-                $html = $dom->saveHTML();
-                var_dump($html);
-                die;
-                modifyUrl($dom);
-                handleStyle($dom);
-                $html = $dom->outertext;
-                $html = modifyStr($html);
-                file_put_contents(TEMP_PATH . '/' . $file, $html);
-                echo $file . LINE;
-            };
+            $filePath = IN_PATH . $file;
+            // if (in_array($tokens[0], $typeList)) {
+            //     save_file(LOG_PATH . 'class.log', "$filePath\n", true);
+            //     $dom = new DOMDocument();
+            //     @$dom->loadHTMLFile($filePath); // html文件载入DOM对象
+            //     $node = $dom->getElementById(substr($file, 0, strlen($file) - 5)); // 获取所需节点
+            //     $content = preg_replace('/ *\n */', '', $dom->saveHTML($node)); // 内容转成1行
+            //     save_file($filePath, $content); // 文件保存到临时目录
+            // };
             // 收集常量
             if ($tokens[count($tokens) - 2] == 'constants') {
-                $content = loadStr($file);
-                $selector = str_get_html($content);
-                $doms = $selector->find('strong code');
-                foreach ($doms as $dom) {
-                    if (!$dom) continue;
-                    $outFile = 'constant.' . $dom->innertext . '.html';
-                    $parent = $dom->parentNode()->parentNode();
-                    $next = $parent->nextSibling();
-                    if (!$next) continue;
-                    $next = $next->children(0);
-                    if (!$next) continue;
-                    modifyUrl($next);
-                    $html = $next->innertext;
-                    if (!$html) continue;
-                    if (trim($html) == '') continue;
-                    $html = modifyStr($html);
-                    if (strpos($outFile, '::')) continue;
-                    echo $outFile . LINE;
-                    file_put_contents(TEMP_PATH . '/' . $outFile, $html);
+                save_file(LOG_PATH . 'constants.log', "$filePath\n", true);
+                $dom = new DOMDocument();
+                @$dom->loadHTMLFile($filePath); // html文件载入DOM对象
+                $nodeList = $dom->getElementsByTagName('strong');
+                foreach ($nodeList as $node) {
+                    $node->
+                    $content = $dom->saveHTML($node);
+                    if (str_starts_with($content, '<strong><code>')) {
+                        echo $content . "\n";
+                    }
                 }
+
+                // $doms = $selector->find('strong code');
+                // foreach ($doms as $dom) {
+                //     if (!$dom) continue;
+                //     $outFile = 'constant.' . $dom->innertext . '.html';
+                //     $parent = $dom->parentNode()->parentNode();
+                //     $next = $parent->nextSibling();
+                //     if (!$next) continue;
+                //     $next = $next->children(0);
+                //     if (!$next) continue;
+                //     modifyUrl($next);
+                //     $html = $next->innertext;
+                //     if (!$html) continue;
+                //     if (trim($html) == '') continue;
+                //     $html = modifyStr($html);
+                //     if (strpos($outFile, '::')) continue;
+                //     echo $outFile . LINE;
+                //     file_put_contents(TEMP_PATH . '/' . $outFile, $html);
+                // }
             }
         }
     }
 }
 
 /**
- * html文件载入DOM对象
- * @param string $fileName
- * @return DOMDocument
- */
-function load_dom(string $fileName): DOMDocument
-{
-    $dom = new DOMDocument();
-    $dom->loadHTMLFile(IN_PATH . $fileName);
-    $dom->normalizeDocument();
-    return $dom;
-}
-
-/**
  * 保存文件
- * @param string $fileName
+ * @param string $filePath
  * @param string $content
  * @param bool $isAppend 是否追加写入 默认false
  * @return void
  */
-function save_file(string $fileName, string $content, bool $isAppend = false)
+function save_file(string $filePath, string $content, bool $isAppend = false)
 {
-    $handle = fopen(TEMP_PATH . $fileName, $isAppend ? 'a+' : 'w+');
+    $handle = fopen($filePath, $isAppend ? 'a+' : 'w+');
     fwrite($handle, $content);
     fclose($handle);
 }
