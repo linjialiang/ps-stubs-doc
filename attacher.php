d6ac81f689724428c80a382ac02f03b3e23a8cd4
linjialiang <linjialiang@163.com>
2023-05-04 23:49:55 +0800

    update

--- attacher.php
+++ attacher.php
@@ -38,9 +38,9 @@ function getComment($file, $oldComment = '', $prefix = '')
     }
 }
 
-function isElement($line, $type): false|string
+function isElement($buffer, $type): false|string
 {
-    $tokens = explode(' ', $line);
+    $tokens = explode(' ', $buffer);
     foreach ($tokens as $k => $v) {
         if ($v == $type && !empty($tokens[$k + 1])) {
             $name = trim($tokens[($k + 1)]);
@@ -50,42 +50,41 @@ function isElement($line, $type): false|string
     return false;
 }
 
-function isConst($line): false|string
+function isConst($buffer): false|string
 {
-    $line = str_replace(' ', '', $line);
+    $buffer = str_replace(' ', '', $buffer);
     $pre = "define('";
-    if (str_starts_with($line, $pre)) {
-        $line = str_replace($pre, '', $line);
-        return explode("'", $line)[0];
+    if (str_starts_with($buffer, $pre)) {
+        $buffer = str_replace($pre, '', $buffer);
+        return explode("'", $buffer)[0];
     }
     return false;
 }
 
-function isVar($line): false|string
+function isVar($buffer): false|string
 {
-    $line = str_replace(' ', '', $line);
+    $buffer = str_replace(' ', '', $buffer);
     $pre = '$';
-    if (str_starts_with($line, $pre)) {
-        $line = str_replace($pre, '', $line);
-        $line = str_replace('_', '', $line);
-        return explode("=", $line)[0];
+    if (str_starts_with($buffer, $pre)) {
+        $buffer = str_replace($pre, '', $buffer);
+        $buffer = str_replace('_', '', $buffer);
+        return explode("=", $buffer)[0];
     }
     return false;
 }
 
 /**
  * 验证是否是注释
- * @param string $line
+ * @param string $buffer
+ * @param bool $passMethod
  * @return bool
  */
-function isComment(string $line): bool
+function isComment(string $buffer, bool $passMethod = false): bool
 {
-    // TODO #[ 开头的有些是在注释下面，有些是在函数和方法里面，待解决
-    // foreach (['/**', '* ', '*/', '#['] as $item) {
     foreach (['/**', '* ', '*/'] as $item) {
-        if (str_starts_with($line, $item)) return true;
+        if (str_starts_with($buffer, $item)) return true;
     }
-    return false;
+    return str_starts_with($buffer, '#[') && !$passMethod;
 }
 
 function handle($filePath): void
@@ -94,42 +93,50 @@ function handle($filePath): void
     if ($fp) {
         $class = '';   // 类名
         $content = ''; // 新的内容
-        $comment = ''; // 旧的注释
+        $oldComment = ''; // 旧的注释
+        $passMethod = false; // 是否到达函数、类方法，用于解决 #[ 在方法内部问题
         // 以只读方式打开一个文件
-        while (false !== ($buffer = fgets($fp, 4096))) {// 从文件指针中读取一行
-            $ltrimBuffe1r = ltrim($buffer); // 处理掉行首空白的行
-            if (empty($handleLine)) {
-                $content .= $buffer . PHP_EOL;
-                $comment = ''; // 所有空白行不会使用到注释，清空旧的注释
-            } elseif (isComment($handleLine)) {// 拿到函数、方法、常量、类等的注释
-                $comment .= $buffer;
+        while (false !== ($buffer = fgets($fp, 4096))) {// 从文件指针中读取一行，带换行符
+            $buffer_ltrim = ltrim($buffer); // 处理掉行首空白的行
+            if (empty($buffer_ltrim)) {
+                $passMethod = false; // 遇到空行将 $passMethod 设为false
+                $content .= $buffer;
+                $oldComment = ''; // 所有空白行不会使用到注释，清空旧的注释
+            } elseif (isComment($buffer_ltrim, $passMethod)) {// 拿到函数、方法、常量、类等的注释
+                $oldComment .= $buffer;
                 // 注释需要后续处理，所以不需要增加新行
             } else {//
                 // ================ 处理注释 start ================ //
-                if ($className = isElement($handleLine, 'class')) {// 类名注释
+                if ($className = isElement($buffer_ltrim, 'class')) {// 类名注释
                     $class = $className;
-                    $newComment = getComment('class.' . $class, $comment);
-                } elseif ($function = isElement($handleLine, 'function')) {// 函数、类方法注释
+                    $newComment = getComment('class.' . $class, $oldComment);
+                } elseif ($function = isElement($buffer_ltrim, 'function')) {// 函数、类方法注释
+                    $passMethod = true;
                     if (str_starts_with($function, 'PS_UNRESERVE_PREFIX_')) $function = substr($function, 20);
                     $blankPre = str_starts_with($buffer, ' ');    // 前面空白是类方法的特征
                     $function = ($class && $blankPre) ? "$class.$function" : "function.$function";
                     $prefix = ($class && $blankPre) ? '    ' : '';
-                    $newComment = getComment($function, $comment, $prefix);
-                } elseif ($const = isConst($handleLine)) {// 常量+注释
-                    $newComment = getComment('constant.' . $const, $comment);
-                } elseif ($var = isVar($handleLine)) {// 预定义变量+注释
-                    $newComment = getComment('reserved.variables.' . $var, $comment);
+                    $newComment = getComment($function, $oldComment, $prefix);
+                } elseif ($const = isConst($buffer_ltrim)) {// 常量+注释
+                    $newComment = getComment('constant.' . $const, $oldComment);
+                } elseif ($var = isVar($buffer_ltrim)) {// 预定义变量+注释
+                    $newComment = getComment('reserved.variables.' . $var, $oldComment);
+                } elseif (str_starts_with($buffer_ltrim, ')')) {
+                    $passMethod = false; // 以 ')' 结尾代表一个方法结束
                 }
                 // ================ 处理注释 end ================ //
-                $content .= $newComment ?? $comment;
+                $content .= $newComment ?? $oldComment;
                 $content .= $buffer . PHP_EOL;
-                $comment = '';    // 旧的注释已使用，清空旧的注释
+                $oldComment = '';    // 旧的注释已使用，清空旧的注释
             }
         }
         // 函数检测是否已到达文件末尾
         if (!feof($fp)) echo "$filePath Error: unexpected fgets() fail\n";
         fclose($fp);
+        // var_dump($content);die;
+        // var_dump($filePath);
         file_put_contents($filePath, $content);
+        // die;
     }
 }
 
