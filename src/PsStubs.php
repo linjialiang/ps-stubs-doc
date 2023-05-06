e7ac7ffadec164375484269e1ce3854a0f413929
linjialiang <linjialiang@163.com>
2023-05-07 00:21:46 +0800

    update

--- src/PsStubs.php
+++ src/PsStubs.php
@@ -25,33 +25,29 @@ class PsStubs
     private const PS_PATH = __DIR__ . '/../raw/phpstorm-stubs';
 
     /**
-     * @var array 类名信息
-     */
-    private array $classInfo = [];
-
-    /**
-     * @var array 函数、方法名信息
-     */
-    private array $methodInfo = [];
-
-    /**
-     * @var bool 注解，注释下的注解归入注释
-     */
-    private bool $isAttribute = false;
-
-    /**
      * @var string 新的内容
      */
-    private string $content = '';
+    private string $content;
 
     /**
      * @var string 旧的注释
      */
-    private string $oldComment = '';
+    private string $oldComment;
 
-    public function __construct()
-    {
-    }
+    /**
+     * @var array 类名信息
+     */
+    private array $classInfo;
+
+    /**
+     * @var array 函数、方法名信息
+     */
+    private array $methodInfo;
+
+    /**
+     * @var bool 注解，注释下的注解归入注释
+     */
+    private bool $isAttribute;
 
     /**
      * 递归获取所有目录
@@ -60,7 +56,7 @@ class PsStubs
      */
     public function run(string $parent = ''): void
     {
-        $handle = @opendir(self::PS_PATH);
+        $handle = @opendir(self::PS_PATH . "/$parent");
         if (!$handle) exit('目录打开失败');
         while (false !== ($file = readdir($handle))) {
             if ('..' === $file || '.' === $file) continue;  // 排除根目录
@@ -68,6 +64,7 @@ class PsStubs
             if (is_dir($filePath)) { // 如果是目录，就进行递归获取文件
                 $this->run("$parent/$file");
             } elseif (str_ends_with($file, '.php')) {// 处理文件
+                var_dump($filePath);
                 $this->handle($filePath);
             }
         }
@@ -83,16 +80,17 @@ class PsStubs
     {
         $fp = @fopen($filePath, 'r');
         if ($fp) {
+            $this->clean();
             // 以只读方式打开一个文件
             while (false !== ($buffer = fgets($fp, 4096))) { // 从文件指针中读取一行，带换行符
                 $buffer_trim = trim($buffer); // 处理掉行首空白的行
-                if (empty($buffer_trim)) {
+                if (empty($buffer_trim)) {// 空白行
                     $this->content .= $this->oldComment; // 可能存在旧的注释
-                    $this->content .= $buffer;     // 保留空白行
-                    $this->oldComment = '';        // 所有空白行不会使用到注释，清空旧的注释
-                    $this->methodInfo = [];     // 遇到空行将 $methodInfo 设为 false
-                    $this->isAttribute = false;     // 非注释将注解信息设为 false
-                } elseif ($this->isComment($buffer_trim)) { // 拿到函数、方法、类的注释
+                    $this->content .= $buffer;           // 保留空白行
+                    $this->oldComment = '';              // 所有空白行不会使用到注释，清空旧的注释
+                    $this->methodInfo = [];              // 遇到空行将 $methodInfo 设为 false
+                    $this->isAttribute = false;          // 非注释将注解信息设为 false
+                } elseif ($this->isComment($buffer_trim)) {// 拿到函数、方法、类的注释
                     $this->oldComment .= $buffer; // 注释需要后续处理，所以不需要增加新行
                 } else {
                     $this->isAttribute = false;     // 非注释将注解信息设为 false
@@ -127,6 +125,19 @@ class PsStubs
             fclose($fp);
             file_put_contents($filePath, $this->content);
         }
+    }
+
+    /**
+     * 清空属性
+     * @return void
+     */
+    private function clean(): void
+    {
+        $this->content = '';
+        $this->oldComment = '';
+        $this->classInfo = [];
+        $this->methodInfo = [];
+        $this->isAttribute = false;
     }
 
     /**
